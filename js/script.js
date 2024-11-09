fd83e5f340965b48c5fdf9cc2d046e66c79ccfb5
AngusPanero <aguspanero@gmail.com>
2024-11-09 15:59:01 +0100

    Terminado 100%

--- js/script.js
+++ js/script.js
@@ -16,16 +16,16 @@ buttons.forEach(button => {
     
     
     switch (jugada) {
-        case "piedra":
-          divResultado.innerHTML = "<p>Elección Usuario: Piedra</p>";
-          break;
-        case "papel":
-            divResultado.innerHTML = "<p>Elección Usuario: Papel</p>";
-          break;
-        case "tijera":
-            divResultado.innerHTML = "<p>Elección Usuario: Tijera</p>";
-          break;
-      }
+    case "piedra":
+    divResultado.innerHTML = "<p>Elección Usuario: Piedra</p>";
+    break;
+    case "papel":
+    divResultado.innerHTML = "<p>Elección Usuario: Papel</p>";
+    break;
+    case "tijera":
+    divResultado.innerHTML = "<p>Elección Usuario: Tijera</p>";
+    break;
+    }
 
 
       const jugadaAle = Math.floor(Math.random() * 3)
@@ -34,13 +34,13 @@ buttons.forEach(button => {
       
       
     switch (jugadaAle) {
-  case 0:
+    case 0:
     divResultado.innerHTML += "<p>Elección PC: Piedra</p>";
     break;
-  case 1:
+    case 1:
       divResultado.innerHTML += "<p>Elección PC: Papel</p>";
     break;
-  case 2:
+    case 2:
       divResultado.innerHTML += "<p>Elección PC: Tijera</p>";
     break;
 }
@@ -49,49 +49,41 @@ jugadaPc()
 
 
 switch (true) {
-  case (jugada == "piedra" && jugadaAle == 0):
+    case (jugada == "piedra" && jugadaAle == 0):
     divResultado.innerHTML += "<p>Han Empatado</p>";
     break;
-  case (jugada == "papel" && jugadaAle == 1):
+    case (jugada == "papel" && jugadaAle == 1):
     divResultado.innerHTML += "<p>Han Empatado</p>";
     break;
-  case (jugada == "tijera" && jugadaAle == 2):
+    case (jugada == "tijera" && jugadaAle == 2):
     divResultado.innerHTML += "<p>Han Empatado</p>";
     break;
 
 
     case (jugada == "piedra" && jugadaAle == 2):
-      divResultado.innerHTML += "<p>Gana el Usuario</p>";
-      puntosUsuario += 1;
-      pUsuario.innerText = `El Usuario Tiene: ${puntosUsuario} puntos`;
-      console.log(puntosUsuario);
-      break;
+    divResultado.innerHTML += "<p>Gana el Usuario</p>";
+    puntosUsuario += 1;
+    pUsuario.innerText = `Usuario Tiene: ${puntosUsuario} puntos`;
+    console.log(puntosUsuario);
+    break;
     case (jugada == "papel" && jugadaAle == 0):
-      divResultado.innerHTML += "<p>Gana el Usuario</p>";
-      puntosUsuario += 1;
-      pUsuario.innerText = `El Usuario Tiene: ${puntosUsuario} puntos`;
-      break;
+    divResultado.innerHTML += "<p>Gana el Usuario</p>";
+    puntosUsuario += 1;
+    pUsuario.innerText = `Usuario Tiene: ${puntosUsuario} puntos`;
+    break;
     case (jugada == "tijera" && jugadaAle == 1):
-      divResultado.innerHTML += "<p>Gana el Usuario</p>";
-      puntosUsuario += 1;
-      pUsuario.innerText = `El Usuario Tiene: ${puntosUsuario} puntos`;
-      break;
+    divResultado.innerHTML += "<p>Gana el Usuario</p>";
+    puntosUsuario += 1;
+    pUsuario.innerText = `Usuario Tiene: ${puntosUsuario} puntos`;
+    break;
 
-default:
-  divResultado.innerHTML += "<p>Gana la PC</p>";
-  puntosPc += 1;
-      pPc.innerText = `La PC Tiene: ${puntosPc} puntos`;
-break;
+    default:
+    divResultado.innerHTML += "<p>Gana la PC</p>";
+    puntosPc += 1;
+    pPc.innerText = `La PC Tiene: ${puntosPc} puntos`;
+    break;
 }
-const resultElement = divResultado;
-resultElement.classList.remove('animacionSuma');
-
-// Force a reflow to reset the animation
-void resultElement.offsetWidth;
-
-// Add the animation class again to trigger the animation
-resultElement.classList.add('animacionSuma');
-  });
+});
 });
 
 
